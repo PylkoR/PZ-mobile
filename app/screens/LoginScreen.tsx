ebe2cc6e1e2b745f4df47a0264b4b6fb2c160b5a
Rafał <rafal.pylko03@gmail.com>
2025-03-04 19:20:14 +0100

    Initial commit - project structure

---
+++ app/screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import ScreenWrapper from "./ScreenWrapper";
+
+export default function LoginScreen() {
+  return (
+    <ScreenWrapper>
+      <Text>Login Screen</Text>
+    </ScreenWrapper>
+  );
+}
