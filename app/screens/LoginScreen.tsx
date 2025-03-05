56243ce1a872c6712773a668b030acb77f7e20cc
Rafał <rafal.pylko03@gmail.com>
2025-03-05 12:15:33 +0100

    KAN-23 added comment

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import { Text } from "react-native";
 import ScreenWrapper from "./ScreenWrapper";
-
+{/*git commit -m "KAN-23 <message>"*/}
 export default function LoginScreen() {
   return (
     <ScreenWrapper>
